aacd3e7a64d9343617f3f1c7d289b1457e306a7d
Xi Xu <i@xi-xu.me>
2024-11-02 19:32:27 +0800

    Update platforms.js

--- src/config/platforms.js
+++ src/config/platforms.js
@@ -5,22 +5,27 @@
 export const PLATFORMS = {
 	/** @type {{base: string, transform: function(string): string}} GitHub configuration */
 	gh: {
-		base: 'https://github.com',
-		transform: (path) => path.replace(/^\/gh\//, '/'),
+		base: "https://github.com",
+		transform: (path) => path.replace(/^\/gh\//, "/"),
 	},
 	/** @type {{base: string, transform: function(string): string}} GitLab configuration */
 	gl: {
-		base: 'https://gitlab.com',
-		transform: (path) => path.replace(/^\/gl\//, '/') + '/raw',
+		base: "https://gitlab.com",
+		transform: (path) => path.replace(/^\/gl\//, "/") + "/raw",
 	},
 	/** @type {{base: string, transform: function(string): string}} Hugging Face configuration */
 	hf: {
-		base: 'https://huggingface.co',
-		transform: (path) => path.replace(/^\/hf\//, '/'),
+		base: "https://huggingface.co",
+		transform: (path) => path.replace(/^\/hf\//, "/"),
 	},
 	/** @type {{base: string, transform: function(string): string}} Kaggle configuration */
 	kg: {
-		base: 'https://www.kaggle.com',
-		transform: (path) => path.replace(/^\/kg\//, '/'),
+		base: "https://www.kaggle.com",
+		transform: (path) => path.replace(/^\/kg\//, "/"),
 	},
+	// /** @type {{base: string, transform: function(string): string}} All platforms */
+	// link: {
+	// 	base: "https://",
+	// 	transform: (path) => path.replace(/^\/link\//, "/"),
+	// },
 };
